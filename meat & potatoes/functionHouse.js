cdb6a0930c88c364b16887fda4b2de05b31cdeee
Dr. Taintlove <sirwilliamwood@gmail.com>
2023-04-10 12:28:49 -0500

    added meat & potatoes data

--- meat & potatoes/functionHouse.js
+++ meat & potatoes/functionHouse.js
@@ -1,1 +1,82 @@
-//code here
+const Boss = require('../cards/boss');
+const Engineer = require('../cards/engineer');
+const Intern = require('../cards/intern');
+const inquirer = require('inquirer');
+const qs = require('./prompts');
+const generateHtml = require('./generateHtml');
+let newBoss = [];
+let newEngineer = [];
+let newIntern = [];
+
+function createMgr(fileName, data) {
+    inquirer
+        .prompt(qs.bossQs)
+        .then((response) => {
+            let mgr = new Boss(response.bossName, response.bossId, response.bossEmail, response.telly,)
+            newBoss.push(mgr);
+            console.log("new boss has arrived!");
+            inquirer
+                .prompt(qs.nextSteps)
+                .then((response) =>
+                optionsResponse(response)
+        );
+        }).catch(error =>{
+            console.log(error);
+        });
+        
+}
+
+function createEng(fileName, data) {
+    inquirer
+        .prompt(qs.engineerQs)
+        .then((response) => {
+            let engineer = new Engineer(response.eName, response.eId, response.eEmail, response.github) 
+            newEngineer.push(engineer);
+            console.log("new engineer joining the team!");
+            inquirer
+                .prompt(qs.nextSteps)
+                .then((response) =>
+                optionsResponse(response)
+        );
+        }).catch(error =>{
+            console.log(error);
+        })
+
+}
+
+function createInt(fileName, data) {
+    inquirer
+        .prompt(qs.internQs)
+        .then((response) => {
+            let intern = new Intern(response.iName, response.iId, response.iEmail, response.school,) 
+            newIntern.push(intern);
+            console.log("unpaid labor! gotta love it");
+            inquirer
+                .prompt(qs.nextSteps)
+                .then((response) =>
+                optionsResponse(response)
+        );
+        }).catch(error =>{
+            console.log(error);
+        })
+}
+
+function optionsResponse(response) {
+    console.log(response.options);
+    let egg = response.options
+    if (egg == 'ADD AN ENGINEER!') {
+        createEng();
+    } else if (egg == 'ADD AN INTERN!') {
+        createInt();
+    } else if (egg == 'FINISH BUILDING MY TEAM') {
+        //generateMarkdown function of new cards
+        generateHtml(newBoss, newEngineer, newIntern)
+    } else {console.log('hmmmmm...something is wrong')
+    }
+}
+
+function init() {
+    createMgr();
+}
+
+module.exports = {init, createMgr};
--- meat & potatoes/prompts.js
+++ meat & potatoes/prompts.js
@@ -1,1 +1,81 @@
-//code here
+const qs = {
+    bossQs: [
+        {
+            type: 'input',
+            message: 'TELL ME WHO DAS BOSS IS YA?',
+            name: 'bossName'
+        },
+        {
+            type: 'input',
+            message: 'VAT IST BOSS ID #?',
+            name: 'bossId'
+        },
+        {
+            type: 'input',
+            message: 'VAT IST BOSS ELECTRONIC MAILING ADDRESS?',
+            name: 'bossEmail'
+        },
+        {
+            type: 'input',
+            message: 'VAT IST BOSS OFFICE PHONE NUMBA?',
+            name: 'telly'
+        }
+    ],
+
+    engineerQs: [
+        {
+            type: 'input',
+            message: 'tell me the name of your engineer...',
+            name: 'eName'
+        },
+    {
+            type: 'input',
+            message: 'vas is engineer id #?',
+            name: 'eId'
+        },
+        {
+            type: 'input',
+            message: 'vat ist engineer email yah?',
+            name: 'eEmail'
+        },
+        {
+            type: 'input',
+            message: 'vat ist das GitHub of das engineer?',
+            name: 'github'
+        }
+    ],
+
+    internQs: [
+        {
+            type: 'input',
+            message: 'tell me the name of your intern...',
+            name: 'iName'
+        },
+        {
+            type: 'input',
+            message: 'vas is intern id #?',
+            name: 'iId'
+        },
+        {
+            type: 'input',
+            message: 'vat ist intern email yah?',
+            name: 'iEmail'
+        },
+        {
+            type: 'input',
+            message: 'Where did your intern attend school @?',
+            name: 'school'
+        }
+    ],
+
+    nextSteps: [
+            {
+            type: 'list',
+            message: 'Whatcha wanna do next?',
+            choices: ['ADD AN ENGINEER!', 'ADD AN INTERN!', 'FINISH BUILDING MY TEAM'],
+            name: 'options'
+        }
+    ]
+};
+
+module.exports = qs;
